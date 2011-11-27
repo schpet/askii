bc81b5e636b6af86d6e597769cb41943651052c9
Peter Schilling <peter@peterschilling.org>
2011-11-26 23:11:04 -0800

    ski trails

--- askii.js
+++ askii.js
@@ -1,39 +1,155 @@
 function Skier(world){
-    this.x = 0;
-    this.width = 6;
+    var self = this;
+
+    this.x = 4;
+    this.y = 4;
+    this.width = 5;
     this.height = 4;
     this.world = world;
 
     this.states = {
         'right': [
-            "..    ", 
-        " ..O  ", 
-        "  -|- ",
-        "   \\\\ "
+                    "  O  ", 
+                    " -|- ",
+                    "  \\\\ ",
+                    "   ``"
             ],
         'left': [
-            "    ..",
-        "  O.. ",
-        " -|-  ",
-        " //   "
+                    "  O  ",
+                    " -|- ",
+                    " //  ",
+                    "``   "
+            ],
+        'down': [
+                    "  O  ",
+                    " -|- ",
+                    " | | ",
+                    " ' ' "
+            ],
+        'up': [
+                    "  O  ",
+                    " -|- ",
+                    " \\ / ",
+                    "  \"  "
             ]
     };
+    this.transformations = {};
+    this.transformations['left'] = function(){ 
+        if(self.x > 0){
+            self.x--;
+        }
+    };
+    this.transformations['right'] = function(){ 
+        if(self.x < self.world.map.maxChars){
+            self.x++;
+        }
+    };
+    this.transformations['up'] = function(){ 
+        if(self.y > 0 ){
+            self.y--;
+        }
+    };
+    this.transformations['down'] = function(){ 
+        if(self.y < self.world.map.lines - self.height){
+            self.y++;
+        }
+    };
 
-    this.state = this.states['right'];
+    this.trackIndent = {};
+    this.trackIndent['left'] = [2, 3];
+    this.trackIndent['right'] = [1, 2];
+    this.trackIndent['down'] = [1, 3];
+    this.trackIndent['up'] = [1, 3];
+    
+    this.state = 'right';
 
-    this.turnLeft = function(){
-        this.state = this.states['left'];
-    }
-    this.turnRight = function(){
-        this.state = this.states['right'];
+    this.getState = function(){
+        return this.states[this.state];
     }
 
     this.stepForward = function(){
-        var max = this.world.map.maxChars;
-        if(this.state == this.states['left'] && this.x > 0)
-            this.x--;
-        else if(this.state == this.states['right'] && this.x < max - this.width)
-            this.x++;
+        var maxX = this.world.map.maxChars;
+        this.transformations[this.state]();
+    }
+
+    this.render = function(map, position){
+        // get 
+        var line = '';
+
+        var original = map[position];
+        if(position < self.y){
+            // long ago 
+            return original;
+        } else if(position >= self.y && position < self.y + self.height ){
+            
+            var offset = position - self.y;
+            console.log(offset);
+
+            if(offset == self.height - 3){
+                //track 
+                var leftTrackPos = self.trackIndent[self.state][0];
+                var rightTrackPos = self.trackIndent[self.state][1];
+                var cursor = self.x + leftTrackPos;
+
+                var trackLine = original.substr(0, cursor);
+                cursor++;
+                trackLine += ".";
+                for(; cursor < self.x + rightTrackPos; cursor++){
+                    trackLine += original.charAt(cursor);
+                }
+                trackLine += ".";
+                cursor++;
+
+                trackLine += original.substr(cursor);
+
+                
+
+                map[position] = trackLine;
+            }
+
+            var skier = self.getState()[offset];
+            var original = map[position];
+
+            line = original.substr(0, self.x);
+            for(var i = 0; i < skier.length; i++){
+                var character = skier.charAt(i);
+                if(character == ' '){
+                    line += original.charAt(self.x + i);
+                } else {
+                    line += '<span class="skier">'; 
+                    line += character;
+                    line += '</span>';
+                }
+            }
+            line += original.substr(self.x + self.width);
+            return line;
+        } else {
+            return map[position];
+        }
+
+/*
+        if(offset == 0){
+            original = "zzzzzzz";
+        } else {
+
+            var skier = self.getState()[offset - 1];
+
+            line = original.substr(0, self.x);
+            for(var i = 0; i < skier.length; i++){
+                var character = skier.charAt(i);
+                if(character == ' '){
+                    line += original.charAt(self.x + i);
+                } else {
+                    line += '<span class="skier">'; 
+                    line += character;
+                    line += '</span>';
+                }
+            }
+            line += original.substr(self.x + self.width);
+        }
+
+*/
+        return line;
     }
 }
 
@@ -104,7 +220,7 @@ function Map(hook){
         for(var i = 0; i < this.maxChars; i++){
             var rand = Math.random()
                 if(rand < 0.99){
-                    s += '`'
+                    s += ' '
                 } else {
                     s += '#'
                 }
@@ -132,24 +248,8 @@ function Game(){
     this.render = function(){
         var visible = "";
 
-
         for(var i = 0; i < this.map.lines; i++){
-            var original = this.map.map[i];
-            var line = undefined;
-
-            if(i >= this.skierPosition && i < this.skierPosition + this.skier.height){
-                var relativePos = i - this.skierPosition;
-
-                line = original.substr(0, this.skier.x);
-                line += '<span class="skier"'; 
-                line += (relativePos == 1) ? ' id="skier">': '>';
-                line += this.skier.state[relativePos];
-                line += '</span>';
-                line += original.substr(this.skier.x + this.skier.width);
-            }  else {
-                line = original;
-            }
-
+            var line = this.skier.render(this.map.map, i);
             visible += line + "\n";
         }
         this.map.hook.html(visible);
@@ -191,22 +291,25 @@ $(window).resize(function() {
     game.map.init();
 });
 
+shortcut.add("left",function() {
+    game.skier.state = 'left';
+});
+shortcut.add("right",function() {
+    game.skier.state = 'right';
+});
+
+shortcut.add("up",function() {
+    game.skier.state = 'up';
+});
+
+shortcut.add("down",function() {
+    game.skier.state = 'down';
+});
+
 $('#left').click(function(){
     game.skier.turnLeft();
 });
-shortcut.add("a",function() {
-    game.skier.turnLeft();
-});
-shortcut.add("left",function() {
-    game.skier.turnLeft();
-});
 $('#right').click(function(){
-    game.skier.turnRight();
-});
-shortcut.add("d",function() {
-    game.skier.turnRight();
-});
-shortcut.add("right",function() {
     game.skier.turnRight();
 });
 
